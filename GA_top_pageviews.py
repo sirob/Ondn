deb732ea96ad5fe28d3ad86d0946a0fd0d21d475
sirob <kryptonnite@gmail.com>
2013-10-24 21:24:16 +0200

    rough version concluded

--- GA_top_pageviews.py
+++ GA_top_pageviews.py
@@ -4,14 +4,18 @@ import re
 
 #Open file and read it as a string.
 
+filename = input("Please provide the path to the Google Analytics file:")
+
 def file_to_string(filename):
-    filename = input("Please provide the path to the Google Analytics file:")
     file = open(filename, 'r')
     text = file.read()
     file.close()
     return text
 
 #Catch names and number of pageviews article pages for the last week with regular expresions.
+
+text = file_to_string(filename)
+
 
 def catch_data(text):
     site_name = re.search('www.\w+.\w+', text)
@@ -23,3 +27,19 @@ def catch_data(text):
     
 
 #Save 20 most viewed pages in a new file.
+
+tup = catch_data(text)
+
+def write_file(filename):
+    filename = input("Please provide the path to the write-to file:")
+    title = tup[0]
+    date_range = tup[1]
+    articles = tup[2]
+    new_articles = articles[:20]
+    file = open(filename, 'w')
+    file.write(title + '\n\n')
+    file.write(date_range + '\n\n')
+    for art in new_articles:
+        file.write(art[0] + ',' + ' ' + art[1] + '\n\n')
+    file.close()
+    
