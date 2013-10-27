2b298320547ae5392b66a6e41d6058127dc58946
sirob <kryptonnite@gmail.com>
2013-10-27 19:15:53 +0100

    added Ondn exceptions and datetime

--- GA_ondn.py
+++ GA_ondn.py
@@ -3,6 +3,7 @@
 #IMPORTANT - Precondition: Report must be exported from GA Pages (english) in .csv format
 
 import re
+from datetime import datetime
 
 #Open file and read it as a string.
 
@@ -11,6 +12,7 @@ def file_to_string(filename):
     text = file.read()
     file.close()
     return text
+
 
 #Catch name, date, title of article and number of pageviews per article with regular expressions.
 
@@ -21,8 +23,19 @@ def catch_data(text):
     date_range = report_date.group()
     articles = re.findall('(/\w+/\D+),"(\w+,\w+)"', text)
     return title, date_range, articles
-    
 
+
+#Remove listed subcategories from the articles list in the tuple.
+
+def clear_subcat(old_tup):
+    title = old_tup[0]
+    date_range = old_tup[1]
+    articles = old_tup[2]
+    clear_articles = [x for x in articles if x[0] not in ('/slovenija/v-ospredju', '/sport/nogomet',
+                                                          '/poslovni/novice', '/sport/hokej-na-ledu') and '/tag/' not in x[0]]
+    return title, date_range, clear_articles
+
+    
 #Save "n" most viewed pages in a new file.
 
 #Precondition: Your original report must contain a minimum of n articles.
@@ -30,11 +43,14 @@ def catch_data(text):
 def write_file(filewrite, n):
     title = tup[0]
     date_range = tup[1]
+    range_start, range_end = date_range[:8], date_range[9:]
+    date_start = datetime.strptime(range_start, '%Y%m%d')
+    date_end = datetime.strptime(range_end, '%Y%m%d')
     articles = tup[2]
     top_articles = articles[:n]
     file = open(filewrite, 'w')
     file.write(title + '\n\n')
-    file.write(date_range + '\n\n')
+    file.write('From: ' + str(date_start) + ' To: ' + str(date_end) + '\n\n')
     for art in top_articles:
         file.write(art[0] + ',' + ' ' + art[1] + '\n\n')
     file.close()
@@ -45,5 +61,7 @@ if __name__ == "__main__":
     filewrite = input("Please provide the path to the write-to file (without the quotes):")
     n = input("The number of the articles with the most pageviews you would like to have in the new report:")
     text = file_to_string(filename)
-    tup = catch_data(text)
-    write_file(filewrite, int(n)) 
+    old_tup = catch_data(text)
+    tup = clear_subcat(old_tup)
+    write_file(filewrite, int(n))
+    print('Done!')
